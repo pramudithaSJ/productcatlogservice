7bd779badc25f741869fd025cf9c9b85966094a7
Pramuditha Jayawardhana <pramusj@Pramudithas-MacBook-Air-2.local>
2024-05-03 22:11:58 +0530

    change route path

--- app.js
+++ app.js
@@ -20,4 +20,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Middleware setup
 app.use(express.json());
-app.use('/api', productRoutes); 
+app.use('/apiV1', productRoutes); 
